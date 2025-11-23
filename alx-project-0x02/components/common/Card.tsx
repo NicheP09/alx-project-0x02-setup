70f6c0a426ba27ca49e526cc2f23b0cfbf3543d6
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-23 13:15:35 +0100

    made correction to import paths

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,11 +1,12 @@
+// components/common/Card.tsx
 import React from 'react';
-import type { CardProps } from '../../interfaces';
+import { type CardProps } from '@/interfaces';
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="border rounded-lg p-4 shadow-sm bg-white">
+    <div className="p-4 border rounded-md shadow-sm bg-white dark:bg-gray-800">
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <p className="text-gray-700">{content}</p>
+      <p className="text-gray-700 dark:text-gray-200">{content}</p>
     </div>
   );
 };
