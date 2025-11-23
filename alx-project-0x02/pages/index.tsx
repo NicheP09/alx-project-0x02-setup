55ac600f35f13b30572fb74fbae26f97b4901b6e
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-23 12:11:12 +0100

    Add alx-project-0x02

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: 'flex',
+        height: '100vh',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'sans-serif',
+      }}
+    >
+      <h1>Welcome to your Next.js Project</h1>
+    </main>
+  );
+}
